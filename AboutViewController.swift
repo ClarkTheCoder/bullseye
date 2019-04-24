c1f871057a914ac2e496f6aedc1b807f056f994c
carson <carsonclark2009@gmail.com>
2019-04-24 00:58:52 -0700

    added about view and view controller

---
+++ AboutViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Carson Clark on 2019-04-24.
+//  Copyright © 2019 com.carson. All rights reserved.
+//
+
+
+// note you must go to attribute selector and connect this view controller to the about view in story board
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    // when user hits close button, send back to main view
+    @IBAction func close(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+        
+        print("close button clicked")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+}
